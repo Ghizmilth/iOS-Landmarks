c0dc33ef2dabf509de8a5859a6503da328f4346d
Hidaner Ferrer <hidanerferrer@gmail.com>
2021-01-15 11:12:47 -0800

    Dynamic previews on different screen size

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,8 +9,6 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-       
-        //
         NavigationView {
             // To create lists from collections, we provide a path to a property
             // that uniquely identifies each element or by making the data conform to Identifiable
@@ -27,6 +25,14 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        /*
+          When the elements of your data are simple value types — like the strings you’re using here — you can use
+          \.self as key path to the identifier.
+         */
+        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: "iPhone SE (2ndgeneration"))
+                .previewDisplayName(deviceName)
+        }
     }
 }
