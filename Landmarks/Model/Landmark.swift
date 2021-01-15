c36fcab62af57f2f2ce0100187cecd33d89b6799
Hidaner Ferrer <hidanerferrer@gmail.com>
2021-01-14 18:12:13 -0800

    Take JSON data and show as view

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -26,10 +26,15 @@ struct Landmark: Hashable, Codable {
 
     // Manage information about Landmark location
     private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+         CLLocationCoordinate2D(
+             latitude: coordinates.latitude,
+             longitude: coordinates.longitude)
+     }
     
     struct Coordinates: Hashable, Codable {
         var latitude: Double
-        var altitude: Double
+        var longitude: Double
     }
 
 
--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -7,36 +7,28 @@
 
 import Foundation
 
-// this is an array of landmarks that come from the .JSON file and it requests data using Landmark.swift struct 
+// this is an array of landmarks that come from the .JSON file and it requests data using Landmark.swift struct
 var landmarks: [Landmark] = load("landmarkData.json")
 
-
 // load(_:) method that fetches JSON  data with a given name from the app's main bundle
-
 func load<T: Decodable>(_ filename: String) -> T {
-    
     let data: Data
-    
+
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
     else {
         fatalError("Couldn't find \(filename) in main bundle.")
     }
-    
-    
+
     do {
         data = try Data(contentsOf: file)
     } catch {
         fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
     }
-    
-    
+
     do {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
     } catch {
         fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
-    
-    
-    
 }
