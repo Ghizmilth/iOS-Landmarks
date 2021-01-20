ac5e66c177a4d51358b56a3e620ed9316b06755c
Hidaner Ferrer <hidanerferrer@gmail.com>
2021-01-20 15:39:52 -0800

    Add Model object to view

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,21 +9,24 @@ import SwiftUI
 
 struct LandmarkList: View {
     
+    
+    @EnvironmentObject var modelData: ModelData
     // This is to declare a State that changes in this view
     @State private var showFavoritesOnly = false
     
     // filtered version of the landmarks list
     var filteredLandmarks:[Landmark] {
-        landmarks.filter { landmark in
+        modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
         }
     }
     
     var body: some View {
         NavigationView {
-            // To create lists from collections, we provide a path to a property
-            // that uniquely identifies each element or by making the data conform to Identifiable
+        // To create lists from collections, we provide a path to a property
+        // that uniquely identifies each element or by making the data conform to Identifiable
             List {
+                // You use the $ prefix to access a binding to a state variable, or one of its properties.
                 Toggle(isOn: $showFavoritesOnly) {
                     Text("Favorites only")
                 }
@@ -48,6 +51,7 @@ struct LandmarkList_Previews: PreviewProvider {
          */
 //        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
             LandmarkList()
+                .environmentObject(ModelData())
 //                .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation"))
 //                .previewDisplayName(deviceName)
 //        }
