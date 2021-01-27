7b502833fd5ce4ddd5fd6eed41af1d94a7eee838
Hidaner Ferrer <hidanerferrer@gmail.com>
2021-01-26 17:46:52 -0800

    Restored LandmarkRow file

--- Landmarks/Views/Landmarks/LandmarkRow.swift
+++ Landmarks/Views/Landmarks/LandmarkRow.swift
@@ -2,42 +2,44 @@
 //  LandmarkRow.swift
 //  Landmarks
 //
-//  Created by Hidaner Ferrer on 1/14/21.
+//  Created by Hidaner Ferrer on 1/26/21.
 //
 
+
+import Foundation
 import SwiftUI
+import CoreLocation //property that is useful to interact with MapKit framework
 
-struct LandmarkRow: View {
-    var landmark: Landmark
-
-    var body: some View {
-        HStack {
-            landmark.image
-                .resizable()
-                .frame(width: 50, height: 50)
-            Text(landmark.name)
-
-            Spacer()
-            
-            if landmark.isFavorite {
-                Image(systemName: "star.fill")
-                    .foregroundColor(.yellow)
-            }
-            
-        }
-    }
-}
-
-struct LandmarkRow_Previews: PreviewProvider {
+// Codabe allows us to get load data into the sttructure from a data file in an easier way
+// This struct is related to the data that comes from the .json file
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    var isFavorite: Bool
     
-    static var landmarks = ModelData().landmarks
-
-    static var previews: some View {
-        Group{
-            LandmarkRow(landmark: landmarks[0])
-            LandmarkRow(landmark: landmarks[1])
-        }
-        .previewLayout(.fixed(width: 300, height: 70))
-       
+    // Computed image proerty to get the image name from the asset catalog
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
     }
+
+    // Manage information about Landmark location
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+         CLLocationCoordinate2D(
+             latitude: coordinates.latitude,
+             longitude: coordinates.longitude)
+     }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+
+
+
 }
+
