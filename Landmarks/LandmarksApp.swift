e12a6a0e8f58b61d873c018dac84e80e1ffe5b5c
Hidaner Ferrer <hidanerferrer@gmail.com>
2021-03-31 10:24:48 -0700

    Create custom notification

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -17,5 +17,10 @@ struct LandmarksApp: App {
             ContentView()
                 .environmentObject(modelData)
         }
+        
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
+        
     }
 }
